1a6c183d790c17e411312c9231bd05908b86631b
Ning Wei <43491290+ningweinw@users.noreply.github.com>
2025-01-17 13:52:07 +0800

    Update unique_courses.rb

--- _plugins/unique_courses.rb
+++ _plugins/unique_courses.rb
@@ -3,17 +3,20 @@ module Jekyll
     safe true
 
     def generate(site)
+      puts "UniqueCoursesGenerator->generate"
       unique_courses = Set.new
 
       site.pages.each do |page|
-        if page['courses']
-          page['courses'].each do |course|
+        puts "#{page.data['name']}, courses=#{page.data['courses']}"
+        if page.data['courses']
+          page.data['courses'].each do |course|
             unique_courses.add(course)
           end
         end
       end
 
       site.data['unique_courses'] = unique_courses.to_a
+      puts "UniqueCoursesGenerator->generate called successfully"
     end
   end
 end
